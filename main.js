573cb2f4c50a132b8f9aeb2c9ecf358a4d08874f
Moses <asugam9@gmail.com>
2024-11-02 23:56:55 +0300

    commit

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ console.log("Good Morning you filth animal");
 // variables
 name = "Mose Asuga"
 console.log(name)
-sentence = "Cant touch this"
+sentence = "lorem  ipsum c owcnwion weuno   "
 console.log(sentence)
 
-a = prompt("Enter a number")
-b = prompt("Enter another number")
+a = 4
+b = 5
 c = a + b
 console.log(c)
+
+//operators
+food = 50
+tip = 0.20
+tipAmount = food * tip
+
+console.log(tipAmount)
+
+// user input
+
+// window.alert(`this is an alert`);
+// var minion=prompt("Enter your prompt");
+
+// document.getElementById("myH1").textContent=`Hello`;
+// document.getElementById("myP").textContent=`I like cartoons`
+
+
+let x;
+let y;
+x = 1003;
+console.log(`Your value is ${x}`)
