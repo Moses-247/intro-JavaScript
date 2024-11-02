f47e6efffe8e9e311b20002d368d250c18a1e049
Moses <asugam9@gmail.com>
2024-11-02 23:24:06 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Good Morning you filth animal");
+
+// variables
+name = "Mose Asuga"
+console.log(name)
+sentence = "Cant touch this"
+console.log(sentence)
+
+a = prompt("Enter a number")
+b = prompt("Enter another number")
+c = a + b
+console.log(c)
