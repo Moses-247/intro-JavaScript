c6da0cb532a0a958f4effaf37b96b0e4066f36fd
Moses <asugam9@gmail.com>
2024-11-16 19:20:09 +0300

    user input

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
-console.log("Good Morning you filth animal");
+// console.log("Good Morning you filth animal");
 
-// variables
-name = "Mose Asuga"
-console.log(name)
-sentence = "lorem  ipsum c owcnwion weuno   "
-console.log(sentence)
+// // variables
+// name = "Mose Asuga"
+// console.log(name)
+// sentence = "lorem  ipsum c owcnwion weuno   "
+// console.log(sentence)
 
-a = 4
-b = 5
-c = a + b
-console.log(c)
+// a = 4
+// b = 5
+// c = a + b
+// console.log(c)
 
-//operators
-food = 50
-tip = 0.20
-tipAmount = food * tip
+// //operators
+// food = 50
+// tip = 0.20
+// tipAmount = food * tip
 
-console.log(tipAmount)
+// console.log(tipAmount)
 
-// user input
+// // user input
 
-// window.alert(`this is an alert`);
-// var minion=prompt("Enter your prompt");
+// // window.alert(`this is an alert`);
+// // var minion=prompt("Enter your prompt");
 
-// document.getElementById("myH1").textContent=`Hello`;
-// document.getElementById("myP").textContent=`I like cartoons`
+// // document.getElementById("myH1").textContent=`Hello`;
+// // document.getElementById("myP").textContent=`I like cartoons`
 
 
-let x;
-let y;
-x = 1003;
-console.log(`Your value is ${x}`)
+// let x;
+// let y;
+// x = 1003;
+// console.log(`Your value is ${x}`)
+
+
+//Accept user input
+
+// let username;
+// username= window.prompt("Enter your username");
+
+// console.log(username)
+
+
+// document.getElementById("MySubmit").onclick = function(){
+//     username = document.getElementById("MyText").value;
+//     document.getElementById("myP").textContent = `Hello ${username}`
+// }
